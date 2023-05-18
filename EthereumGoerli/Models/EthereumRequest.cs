12c64a46a9d630c511238998cd36859595c1a4ed
Mikhail-Kavzov <kavzovm@mail.ru>
2023-05-19 01:07:02 +0300

    Modify EthereumRequest structure

--- EthereumGoerli/Models/EthereumRequest.cs
+++ EthereumGoerli/Models/EthereumRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -15,19 +16,20 @@ namespace EthereumGoerli.Models
         [JsonPropertyName("method")]
         public string Method { get; }
         [JsonPropertyName("params")]
-        public Dictionary<string, string> Params { get; }
+        public Dictionary<string,string>[] Params { get; }
         [JsonPropertyName("jsonrpc")]
         public string JsonRPC { get; }
 
         public EthereumRequest(int id, string jsonRPC, string method,
-            Dictionary<string, string> methodParams)
+            Dictionary<string, string>[] methodParams)
         {
             Id = id;
             Method = method;
             Params = methodParams;
             JsonRPC = jsonRPC;
         }
-        public EthereumRequest(string method, Dictionary<string, string> methodParams)
+
+        public EthereumRequest(string method, Dictionary<string, string>[] methodParams)
             : this(1, "2.0", method, methodParams)
         { }
     }
