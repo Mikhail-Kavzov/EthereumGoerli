8fb8e7f4fd2d2cd69c452a2a6c97b07de3fefe5a
Mikhail-Kavzov <kavzovm@mail.ru>
2023-05-19 00:06:44 +0300

    add EthereumRequest model

---
+++ EthereumGoerli/Models/EthereumRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EthereumGoerli.Models
+{
+
+    public class EthereumRequest
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; }
+        [JsonPropertyName("method")]
+        public string Method { get; }
+        [JsonPropertyName("params")]
+        public Dictionary<string, string> Params { get; }
+        [JsonPropertyName("jsonrpc")]
+        public string JsonRPC { get; }
+
+        public EthereumRequest(int id, string jsonRPC, string method,
+            Dictionary<string, string> methodParams)
+        {
+            Id = id;
+            Method = method;
+            Params = methodParams;
+            JsonRPC = jsonRPC;
+        }
+        public EthereumRequest(string method, Dictionary<string, string> methodParams)
+            : this(1, "2.0", method, methodParams)
+        { }
+    }
+}
